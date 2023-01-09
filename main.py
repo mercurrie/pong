147e925dd0d5bf128bdb9a4a59775737da947613
Mason C <43713560+mercurrie@users.noreply.github.com>
2023-01-08 22:21:30 -0600

    complete
    added scoreboard.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import time
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
-import time
+from scoreboard import Scoreboard
 
 direction = 10
 screen = Screen()
@@ -11,36 +11,47 @@ screen.title("Pong")
 screen.bgcolor("black")
 screen.tracer(0)
 
-paddle_1 = Paddle((350, 0))
-paddle_2 = Paddle((-350, 0))
+right_paddle = Paddle((350, 0))
+left_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey(paddle_1.go_up, "Up")
-screen.onkey(paddle_1.go_down, "Down")
-screen.onkey(paddle_2.go_up, "w")
-screen.onkey(paddle_2.go_down, "s")
+screen.onkey(right_paddle.go_up, "Up")
+screen.onkey(right_paddle.go_down, "Down")
+screen.onkey(left_paddle.go_up, "w")
+screen.onkey(left_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.1)
-    ball.move(direction)
+    time.sleep(ball.move_speed)
+    ball.move()
 
     # detect collision with walls
     if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce()
+        ball.bounce_y()
 
+    # detect collision with paddle
+    if ball.distance(right_paddle) < 50 and ball.xcor() > 320 or ball.distance(left_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
 
+    # detect if R paddle misses
+    if ball.xcor() > 380:
+        ball.reset()
+        scoreboard.add_left_score()
+
+    # detect if L paddle misses
+    if ball.xcor() < -380:
+        ball.reset()
+        scoreboard.add_right_score()
 
 screen.exitonclick()
-
-
 
 # TODO: Create screen
 # TODO: Create and move paddles
 # TODO: Create another paddle
 # TODO: Create ball and move it
-# TODO: Detect collison of wall and bounce
+# TODO: Detect collision of wall and bounce
 # TODO: Detect when paddle misses
 # TODO: keep score
--- paddle.py
+++ paddle.py
@@ -11,10 +11,14 @@ class Paddle(Turtle):
         self.color("white")
         self.goto(cord)
 
+    # go_up() moves paddle by 20 pixels if below top of screen
     def go_up(self):
-        new_y = self.ycor() + 20
-        self.goto(self.xcor(), new_y)
+        if self.ycor() < 250:
+            new_y = self.ycor() + 20
+            self.goto(self.xcor(), new_y)
 
+    # go_down() moves paddle down by 20 pixel if above bottom of screen
     def go_down(self):
-        new_y = self.ycor() - 20
-        self.goto(self.xcor(), new_y)
+        if self.ycor() > -250:
+            new_y = self.ycor() - 20
+            self.goto(self.xcor(), new_y)
